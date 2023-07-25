552309e4c7d38791d94aad05018b7b4d90affb0e
gostraa <mcherkashina77@gmail.com>
2023-07-25 20:45:02 +0200

    2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+// Импортируем необходимые зависимости
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+  player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            
+            break;
+
+        default:
+            
+            break;
+    }
+});
+
+player.on("timeupdate", throttle(updateTime, 1000));
+
+function updateTime(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+    form.addEventListener('input', throttle(e => {
+      const key = e.target.name;
+      const value = e.target.value;
+      localStorage.setItem(key, value);
+    }, 500));
+
+    form.addEventListener('submit', e => {
+      localStorage.removeItem("email");
+      localStorage.removeItem("message");
+    });
+
+
+     
+    const email = localStorage.getItem("email");
+    const message = localStorage.getItem("message");
+
+    form.elements.email.value = email;
+    form.elements.message.value = message;
+  
+
+
+    
