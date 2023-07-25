38c5b47a63085cd6cec8c32b76d53b087fbff1f2
gostraa <mcherkashina77@gmail.com>
2023-07-25 23:21:51 +0200

    3 update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,39 @@
 import throttle from 'lodash.throttle';
-
-const form = document.querySelector('.feedback-form');
-
-    form.addEventListener('input', throttle(e => {
-      const key = e.target.name;
-      const value = e.target.value;
-      localStorage.setItem(key, value);
-    }, 500));
-
-    form.addEventListener('submit', e => {
-      localStorage.removeItem("email");
-      localStorage.removeItem("message");
-    });
+import { loadFromLS, saveToLS } from './helpers.js'; //імпортую допоміжні функції
 
 
-     
-    const email = localStorage.getItem("email");
-    const message = localStorage.getItem("message");
+const refs = {
+  form: document.querySelector('.feedback-form'), //ініціалізую форму 
+};
 
-    form.elements.email.value = email;
-    form.elements.message.value = message;
-  
+
+refs.form.addEventListener('input', throttle(e => { //прослуховувач подій для форми під час введення даних
+  const key = e.target.name;     // отримую назву ключа поточного елементу 
+  const value = e.target.value;  // отримую значення поточного елементу
+  saveToLS(key, value);
+},500));
+
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = e.target.elements.email.value;
+  const message = e.target.elements.message.value;
+
+  console.log({ email, message });
+
+  localStorage.clear();
+  e.target.reset();
+});
+
+function onPageLoad() {
+  const email = loadFromLS('email');
+  const message = loadFromLS('message');
+
+  refs.form.elements.email.value = email || 'Anonym@com';
+  refs.form.elements.message.value = message || '';
+}
+onPageLoad();
 
 
     
