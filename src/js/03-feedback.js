11b5ac56b8979da6cbf46e27af09f6e5fba33f6b
gostraa <mcherkashina77@gmail.com>
2023-07-25 23:43:47 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,30 +10,31 @@ const refs = {
 refs.form.addEventListener('input', throttle(e => { //прослуховувач подій для форми під час введення даних
   const key = e.target.name;     // отримую назву ключа поточного елементу 
   const value = e.target.value;  // отримую значення поточного елементу
-  saveToLS(key, value);
+  saveToLS(key, value);          // імпортована фунція яка перетворює інфу в рядок та зберігає в локал сторедж
 },500));
 
 
-refs.form.addEventListener('submit', e => {
-  e.preventDefault();
+refs.form.addEventListener('submit', e => {    //слухач на відправку 
+  e.preventDefault();    // переривання дій браузера по дефолту                       
 
-  const email = e.target.elements.email.value;
-  const message = e.target.elements.message.value;
+  const email = e.target.elements.email.value;  //значення з поля email
+  const message = e.target.elements.message.value; //значення з поля message
 
-  console.log({ email, message });
+  console.log({ email, message }); //вивожу значення в консоль
 
-  localStorage.clear();
-  e.target.reset();
+  localStorage.clear(); //очищую локал сторедж бо вже натиснута кнопка відправити
+  e.target.reset();     //очищую поля форми
 });
 
-function onPageLoad() {
-  const email = loadFromLS('email');
-  const message = loadFromLS('message');
+function onPageLoad() {        //функція для автоматичного заповнення полів вводу при оновленні сторінки 
 
-  refs.form.elements.email.value = email || 'Anonym@com';
-  refs.form.elements.message.value = message || '';
+  const email = loadFromLS('email'); // дістаю з локал стореджу значення поля email (імпортована ф-ція)
+  const message = loadFromLS('message'); // дістаю з локал стореджу значення поля message (імпортована ф-ція)
+
+  refs.form.elements.email.value = email || 'Anonym@com'; //записую в поле email значення email або дефолтне Anonym@com
+  refs.form.elements.message.value = message || ''; //записую в поле message значення email або дефолтне ''
 }
-onPageLoad();
+onPageLoad(); // одразу викликаю щоб функція працювала при завантаженні сторінки
 
 
     
